cd4673427ed894652b5c8417da9a44dfc65e9459
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2026-03-30 10:19:58 +0900

    feat: add sitemap integration and update dependencies; remove Google site verification file

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
-
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
@@ -25,5 +25,5 @@ export default defineConfig({
     ]
   },
 
-  integrations: [mdx()]
+  integrations: [mdx(), sitemap()]
 });
